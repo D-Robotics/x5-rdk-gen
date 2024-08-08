9bf868041fa6bf7be4d21fbaf261dd548437d2ec
jinbao.he <jinbao.he@d-robotics.cc>
2024-08-08 22:21:16 +0800

    fix(mk_debs.sh): make install driver lib to /debian/usr/hobot/lib/
    Signed-off-by: jinbao.he <jinbao.he@d-robotics.cc>

--- mk_debs.sh
+++ mk_debs.sh
@@ -345,18 +345,23 @@ function make_debian_deb() {
 
         # set Depends
         sed -i 's/Depends: .*$/Depends: hobot-boot/' ${deb_dst_dir}/DEBIAN/control
+        cd ${debian_src_dir}/${pkg_name}/drivers/sensor
 
-        #cd ${debian_src_dir}/${pkg_name}/drivers
-        #make || {
-        #    echo "make failed"
-        #    exit 1
-        #}
+        make || {
+           echo "make failed"
+           exit 1
+        }
+
+        make install || {
+           echo "make failed"
+           exit 1
+        }
 
         mkdir -p $deb_dst_dir/app/
         cp -ar ${debian_src_dir}/${pkg_name}/tuning_tool $deb_dst_dir/app/
 
         mkdir -p "$deb_dst_dir/usr/bin/"
-        cp ${debian_src_dir}/${pkg_name}/debian/usr/hobot/lib/* "$deb_dst_dir/usr/hobot/lib/"
+        cp ${debian_src_dir}/${pkg_name}/debian/usr/hobot/lib/* "$deb_dst_dir/usr/hobot/lib/" -a
         is_allowed=1
         ;;
     hobot-dnn)
