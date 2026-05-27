84294c1e172c58f3ba09596ba24ee3b2499eb319
zhongyuan.yao <zhongyuan.yao@d-robotics.cc>
2026-05-27 16:20:55 +0800

    Signed-off-by: zhongyuan.yao <zhongyuan.yao@d-robotics.cc>
    perf(mk_debs.h mk_kernel.sh mk_kernel_rt.sh): [RDK-1498] Use the compressed kernel Image.lz4 to optimize the boot speed

--- mk_debs.sh
+++ mk_debs.sh
@@ -175,8 +175,8 @@ function make_debian_deb() {
 
         boot_dest_dir=${deb_dst_dir}/boot
         mkdir -p "${boot_dest_dir}"
-        cp -arf "${IMAGE_DEPLOY_DIR}"/kernel/Image  "${boot_dest_dir}"/
-        cp -arf "${IMAGE_DEPLOY_DIR}"/kernel/Image-rt  "${boot_dest_dir}"/ || true
+        cp -arf "${IMAGE_DEPLOY_DIR}"/kernel/Image.lz4  "${boot_dest_dir}"/
+        cp -arf "${IMAGE_DEPLOY_DIR}"/kernel/Image-rt.lz4  "${boot_dest_dir}"/ || true
         cp -arf "${KERNEL_DEPLOY_DIR}"/modules/* "${deb_dst_dir}"/
         cp -arf "${debian_src_dir}"/"${pkg_name}"/debian/boot/boot.scr "${deb_dst_dir}"/boot/
         case "$RDK_SOC_NAME" in
